cb2da0cbf533092d58cfa283e159514fd347a679
Miguel Angel <miguelpositive@gmail.com>
2025-03-19 14:12:24 -0500

    agregue el cifrado de passwords para usuarios

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,68 +1,55 @@
-const {usersModel} = require("../models/usersModels.js");
+const { usersModel } = require("../models/usersModels.js");
+const bcrypt = require("bcrypt");
 
-const createUser = async (req, res) =>{
+const ecnryptPass = async (password) => {
+  const saltRounds = 10; // Número de rondas de cifrado (mayor = más seguro)
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+};
 
-try {
-    
-    const {user, password} = req.body;
+// contraseña ingresada, contraseña guardada -- las compara y verifica si son las mismas
+const verifyEncryptedPass = async (passwordEntered, passwordSaved) => {
+  const isOk = await bcrypt.compare(passwordEntered, passwordSaved);
+  return isOk;
+};
 
-    console.log(user);
-    console.log(password);
-    console.log();
-    console.log('====================================');
+const createUser = async (req, res) => {
+  try {
+    const { user, password } = req.body;
 
-    const newUser = await usersModel({user, password});
-    
+    newPass = await ecnryptPass(password);
+
+    const newUser = await usersModel({ user, password: newPass });
+
     await newUser.save();
 
+    console.log("ejecion exitosa");
 
-    console.log("ejecion exitosa")
-    
     res.sendStatus(200);
-
-} catch (error) {
-
-
-   console.log("Error en el backend al intentar crear el usuario "+error)
-    
-}
-
-
-}
+  } catch (error) {
+    console.log("Error en el backend al intentar crear el usuario " + error);
+  }
+};
 
 const validateUser = async (req, res) => {
-
-try {
-
-    const {user, password} = req.body;
+  try {
+    const { user, password } = req.body;
 
     // verifica que el usuario exista y devuelve un objeto
-    const userFound = await usersModel.findOne({user});
+    const userFound = await usersModel.findOne({ user });
 
+    // si no existe el usuario se envia un mensaje donde se dice que no se encontro
 
-    // si no existe el usuario se envia un mensaje donde se dice que no se encontro 
-
-    if (!userFound){
-
-        return res.status(404).json({ message: "Usuario no encontrado" });
-
-    } else if (userFound.password == password) {
-
-        return res.status(200).json({ message: "validación correcta" });
-
+    if (!userFound) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    } else if (await verifyEncryptedPass(password, userFound.password)) {
+      return res.status(200).json({ message: "validación correcta" });
     } else {
-        return res.status(404).json({ message: "contraseña incorrecta" });
-
+      return res.status(404).json({ message: "contraseña incorrecta" });
     }
-    
-} catch (error) {
- 
-    
-   console.log("ocurrio un error al intentar validar los usuarios"+error)
-}
+  } catch (error) {
+    console.log("ocurrio un error al intentar validar los usuarios" + error);
+  }
+};
 
-
-}
-
-
-module.exports = {createUser, validateUser};
+module.exports = { createUser, validateUser };
