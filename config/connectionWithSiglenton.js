71d5eb7a17dc9805d02ab05f929fb7d15a1dfaf2
Miguel Angel <miguelpositive@gmail.com>
2025-04-20 17:17:14 -0500

    solucione un error de la base de datos

--- config/connectionWithSiglenton.js
+++ config/connectionWithSiglenton.js
@@ -12,13 +12,10 @@ class connectionWithSiglenton {
 
   async _connect() {
     try {
-      this.connection = await mongoose.connect(
-        "mongodb://localhost:27017/miDB",
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        }
-      );
+      this.connection = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
       console.log("✅ Conectado a MongoDB");
     } catch (error) {
       console.error("❌ Error al conectar a MongoDB:", error);
